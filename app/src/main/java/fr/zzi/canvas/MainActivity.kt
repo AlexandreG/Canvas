c80e472fc2c4b024f5eefe5c648034b0be6b80e2
Alexandre G <g@zzi.fr>
2018-12-08 02:53:06 +0100

    Add loader

--- app/src/main/java/fr/zzi/canvas/MainActivity.kt
+++ app/src/main/java/fr/zzi/canvas/MainActivity.kt
@@ -8,21 +8,27 @@ import fr.zzi.canvas.model.Pixel
 import fr.zzi.canvas.model.PixelColor
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity(), CanvasView.Callback {
 
-    lateinit var canvasView: CanvasView
-    lateinit var pixelLiveData: PixelLiveData
+    private lateinit var canvasView: CanvasView
+    private lateinit var progressBar: View
+
+    private lateinit var pixelLiveData: PixelLiveData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initCanvas()
+        initCanvasView()
         initLiveData()
+        displayLoader()
     }
 
-    private fun initCanvas() {
+    private fun displayLoader() {
+        progressBar = findViewById(R.id.progress_bar)
+    }
+
+    private fun initCanvasView() {
         canvasView = findViewById(R.id.canvas_view)
         canvasView.callback = this
     }
@@ -30,13 +36,17 @@ class MainActivity : AppCompatActivity(), CanvasView.Callback {
     private fun initLiveData() {
         pixelLiveData = PixelLiveData()
         pixelLiveData.observe(this, Observer { it ->
-            it?.let {
+            if (it?.isNotEmpty() == true) {
+                if (progressBar.visibility == View.VISIBLE) {
+                    progressBar.visibility = View.GONE
+                }
+
                 canvasView.refresh(it)
             }
         })
     }
 
-    public fun updateColor(view: View?) {
+    fun updateColor(view: View?) {
         canvasView.currentColor = when (view?.id) {
             R.id.color_1 -> PixelColor.WHITE
             R.id.color_2 -> PixelColor.GREEN
